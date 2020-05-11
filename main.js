0e6bae287344eea30a859ab4026b0ce08a493ba2
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-05-11 22:50:11 +0700

    fix all code to be shorten

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  const currencyRatioVNDtoUSD = 23388.85;
-  const currencyRatioUSDtoEUR = 1.08;
-  const currencyRatioVNDtoEUR=25321.21;
 
-  function formatCurrency(type1,type2, value) {
+  const USD = {
+    VND: 23388.85,
+    EUR: 0.92,
+    KRW: 1223.56,
+    USD: 1,
+    JPY:107.64,
+    CNY:7.10
+
+  }
+
+  function formatCurrency(type1, type2, value) {
     const formatter = new Intl.NumberFormat(type1, {
       currency: type2,
       style: "currency"
@@ -11,43 +18,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     return formatter.format(value);
   }
 
-  function USDToVND(amount) {
-    // return currencyRatioVNDtoUSD.toFixed(2);
-    return formatCurrency('vi',"VND",currencyRatioVNDtoUSD.toFixed(2)*amount)
-  }
-  function VNDToUSD(amount) {
-    // return (currencyRatioVNDtoUSD / amount).toFixed(2)
-    return formatCurrency('en-US',"USD",(amount/currencyRatioVNDtoUSD).toFixed(2))
-  }
-
-
-  function USDToEUR(amount) {
-    // return currencyRatioVNDtoUSD.toFixed(2);
-    return formatCurrency('de-DE',"EUR",(amount/currencyRatioUSDtoEUR).toFixed(2))
-  }
-  function EURToUSD(amount) {
-    // return (currencyRatioVNDtoUSD / amount).toFixed(2)
-    
-
-    return formatCurrency('en-US',"USD",currencyRatioUSDtoEUR.toFixed(2)*amount)
-  }
-
-
-
-
-  function EURToVND(amount) {
-    // return currencyRatioVNDtoUSD.toFixed(2);
-    return formatCurrency('vi',"VND",currencyRatioVNDtoEUR.toFixed(2)*amount)
-  }
-  function VNDToEUR(amount) {
-    // return (currencyRatioVNDtoUSD / amount).toFixed(2)
-    return formatCurrency('de-DE',"EUR",(amount/currencyRatioVNDtoEUR).toFixed(2))
-  }
-
-
 
   const exchangeBtn = document.getElementById("exchange");
-  const convertBtn = document.getElementById("convertCurrencyBtn");
   exchangeBtn.addEventListener("click", () => {
     var currency1 = document.getElementById("currency1").value;
     var currency2 = document.getElementById("currency2").value;
@@ -55,26 +27,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("currency2").value = currency1
   })
 
-  convertBtn.addEventListener("click", () => {
-    var currency1 = document.getElementById("currency1").value;
-    var currency2 = document.getElementById("currency2").value;
-    const amountInput = document.getElementById("amountInput").value;
-    amountInput ?"" :alert("You must enter an amount");
-    
-    var result="";
-    currency1===currency2 ?result="Currency 1 is the same as Currency 2 :(( Pls re-select. Thank u !" :""
-    currency1==="USD" && currency2==="VND" ? result=(`${amountInput} ${currency1} to ${currency2} is ${USDToVND(amountInput)}`) :""
-    currency1==="VND" && currency2==="USD" ? result=(`${amountInput} ${currency1} to ${currency2} is ${VNDToUSD(amountInput)}`) :""
-
-    currency1==="EUR" && currency2==="USD" ? result=(`${amountInput} ${currency1} to ${currency2} is ${EURToUSD(amountInput)}`) :""
-    currency1==="USD" && currency2==="EUR" ? result=(`${amountInput} ${currency1} to ${currency2} is ${USDToEUR(amountInput)}`) :""
-
-    currency1==="EUR" && currency2==="VND" ? result=(`${amountInput} ${currency1} to ${currency2} is ${EURToVND(amountInput)}`) :""
-    currency1==="VND" && currency2==="EUR" ? result=(`${amountInput} ${currency1} to ${currency2} is ${VNDToEUR(amountInput)}`) :""
-
-    document.getElementById("demo").innerHTML = result;
-  })
   
+  const convertBtn = document.getElementById("convertCurrencyBtn");
+  convertBtn.addEventListener("click", () => {
+    var amountInput = document.getElementById("amountInput").value;
+    amountInput ?"" :alert("U must enter a number to convert");
+    const fromValue = document.getElementById("currency1").value;
+    const toValue = document.getElementById("currency2").value;
+    const result = ((USD[toValue] / USD[fromValue]) * amountInput).toFixed(2)
+    const notification = showResult(toValue, result)
+    document.getElementById("demo").innerHTML = `${amountInput} ${fromValue}=${notification} ${toValue}` ;
+
+
+  })
+
+  const showResult = (toValue, result) => {
+    var convertedValue = "";
+    toValue === "USD" ? convertedValue = formatCurrency('en-US', "USD", result) : ""
+    toValue === "VND" ? convertedValue = formatCurrency('vi', "VND", result) : ""
+    toValue === "EUR" ? convertedValue = formatCurrency('de-DE', "EUR", result) : ""
+    toValue === "JPY" ? convertedValue = formatCurrency('ja-JP', "JPY", result) : ""
+    toValue === "CNY" ? convertedValue = formatCurrency('zh-CN', "CNY", result) : ""
+    return convertedValue;
+  } 
+
 
 
 });
+
